33cb63e970c5cd60980e714f4e5da8a7901fc64a
Riv2oo4 <riv22500@uvg.edu.gt>
2024-11-15 01:26:09 -0600

    Update framebuffer.rs

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,4 +1,6 @@
 use crate::color::Color;
+use crate::obj::Obj;
+use nalgebra_glm::{Mat4, Vec3, Vec4};
 
 pub struct Framebuffer {
     pub width: usize,
@@ -19,26 +21,37 @@ impl Framebuffer {
         self.buffer.fill(0x000000); // Negro
     }
 
-    pub fn draw_circle(&mut self, cx: i32, cy: i32, radius: i32, color: Color) {
-        let mut x = 0;
-        let mut y = radius;
-        let mut d = 1 - radius;
+    pub fn render_model(
+        &mut self,
+        model: &Obj,
+        model_matrix: &Mat4,
+        view_matrix: &Mat4,
+        projection_matrix: &Mat4,
+        color: Color,
+    ) {
+        let mvp = projection_matrix * view_matrix * model_matrix;
 
-        while x <= y {
-            for dx in [-x, x].iter() {
-                for dy in [-y, y].iter() {
-                    self.set_pixel(cx + dx, cy + dy, color.to_hex());
-                    self.set_pixel(cx + dy, cy + dx, color.to_hex());
-                }
+        for vertex in &model.vertices {
+            // Convertir el vector 3D en un vector homogéneo (4D)
+            let vertex_homogeneous = Vec4::new(vertex.x, vertex.y, vertex.z, 1.0);
+            let transformed = mvp * vertex_homogeneous;
+
+            if transformed.w == 0.0 {
+                continue;
             }
 
-            x += 1;
-            if d < 0 {
-                d += 2 * x + 1;
-            } else {
-                y -= 1;
-                d += 2 * (x - y) + 1;
-            }
+            // Convertir coordenadas homogéneas en Normalized Device Coordinates (NDC)
+            let ndc = Vec3::new(
+                transformed.x / transformed.w,
+                transformed.y / transformed.w,
+                transformed.z / transformed.w,
+            );
+
+            // Convertir NDC en coordenadas de pantalla
+            let screen_x = ((ndc.x + 1.0) * 0.5 * self.width as f32) as i32;
+            let screen_y = ((1.0 - ndc.y) * 0.5 * self.height as f32) as i32;
+
+            self.set_pixel(screen_x, screen_y, color.to_hex());
         }
     }
 
