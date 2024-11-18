204d3236025870035312e49aebfd16e121ceb639
Riv2oo4 <riv22500@uvg.edu.gt>
2024-11-17 21:59:35 -0600

    Update planet.rs

--- src/planet.rs
+++ src/planet.rs
@@ -1,40 +1,37 @@
-use nalgebra_glm::{Vec3, Mat4};
+use nalgebra_glm::{ Vec3,Mat4};
+
 
 pub struct Planet {
-    pub name: String,
-    pub distance_from_sun: f32,
-    pub orbit_speed: f32,
-    pub rotation_speed: f32,
-    pub size: f32,
     pub position: Vec3,
+    pub orbit_radius: f32,
+    pub orbit_speed: f32,
+    pub current_angle: f32,
+    pub size: f32,
+    pub shader_index: usize,
 }
 
 impl Planet {
-    pub fn new(name: &str, distance_from_sun: f32, orbit_speed: f32, rotation_speed: f32, size: f32) -> Self {
+    pub fn new(orbit_radius: f32, orbit_speed: f32, size: f32, shader_index: usize) -> Self {
         Planet {
-            name: name.to_string(),
-            distance_from_sun,
+            position: Vec3::zeros(),
+            orbit_radius,
             orbit_speed,
-            rotation_speed,
+            current_angle: 0.0,
             size,
-            position: Vec3::new(distance_from_sun, 0.0, 0.0),
+            shader_index,
         }
     }
 
-    pub fn update_position(&mut self, time: f32) {
-        let angle = time * self.orbit_speed;
+    pub fn update_position(&mut self, delta_time: f32) {
+        self.current_angle += self.orbit_speed * delta_time;
         self.position = Vec3::new(
-            self.distance_from_sun * angle.cos(),
+            self.orbit_radius * self.current_angle.cos(),
             0.0,
-            self.distance_from_sun * angle.sin(),
+            self.orbit_radius * self.current_angle.sin(),
         );
     }
 
-    pub fn get_model_matrix(&self, time: f32) -> Mat4 {
-        let translation = Mat4::new_translation(&self.position);
-        let rotation = Mat4::from_rotation_y(time * self.rotation_speed);
-        let scaling = Mat4::new_scaling(self.size);
-
-        translation * rotation * scaling
+    pub fn get_model_matrix(&self) -> Mat4 {
+        Mat4::new_translation(&self.position) * Mat4::new_scaling(self.size)
     }
 }
